42b16b45fe3b32bd75357c6b1e7e900166b2a949
Daniil Sobol <sobol.daniil@gmail.com>
2013-10-17 17:49:24 +0400

    using same code for both tests

--- seq.js
+++ seq.js
@@ -1,38 +1,5 @@
-var util = require('util');
+var test = require('./testsuite');
 var Seq = require('seq');
 
-Seq()
-  .seq(function () {
-    console.log('seq function1');
-    this(null, 'ololo');
-  })
-  .par(function (arg) {
-    var self = this;
-    console.log('par function1: ' + arg);
-    setTimeout(function () {
-      self(null, 'par1', 'optionl1');
-    }, 200);
-  })
-  .par(function (arg) {
-    var self = this;
-    console.log('par function2: ' + arg);
-    setTimeout(function () {
-      self(null, 'par2', 'optionl2');
-    }, 100);
-  })
-  .seq(function (arg1, arg2) {
-    console.log(util.inspect(arg1));
-    console.log('seq function2. arg1: ' + arg1 + ', arg2: ' + arg2);
-    this();
-  })
-  .seq(function () {
-    console.log('seq function3');
-    this(null, 'seq3');
-  })
-  .seq(function (arg) {
-    console.log('seq function4: ' + arg);
-    this(null, 'OMG!');
-  })
-  .catch(function (e) {
-    console.log('error caught!: ' + e);
-  });
+test.Seq = Seq;
+test.Test();
