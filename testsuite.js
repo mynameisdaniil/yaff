d6581256f924a27cf62d5a70f81a6546db1b98a4
Daniil <sobol.daniil@gmail.com>
2015-07-28 23:53:34 +0300

    improved test

--- testsuite.js
+++ testsuite.js
@@ -3,23 +3,24 @@ var log  = console.log;
 var err  = console.error;
 var fs   = require('fs');
 var path = require('path');
-var maybe = require('maybe2');
 
 
 exports.Seq;
 exports.Test = function () {
   var fs = require('fs');
   this.Seq([1,2,3,4,5,6,7])
-    .parEach(function (num) {
-      if (num % 2 == 0)
-        return this('ha-ha! ' + num);
-      this(null, num);
+    .parMap(function (num) {
+      setTimeout(function () {
+        // if (num % 2 == 0)
+        //   return this('ha-ha! ' + num);
+        this(null, num);
+      }.bind(this), 100);
     })
     .seq(function () {
       log('regual seq', arguments);
       this();
     })
     .finally(function (e, res) {
-      log('finally:', arguments);
+      log('finally. e:', e, '\tres:', res);
     })
 };
